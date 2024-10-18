bd9b20502c7a23a37150be582711998bed336da3
eidolon1302 <gigantician13@gmail.com>
2024-10-19 02:55:29 +0700

    feat: add chart at sidebar

--- client/providers/MainLayout.tsx
+++ client/providers/MainLayout.tsx
@@ -5,7 +5,7 @@ interface MainLayoutProps {
     children: React.ReactNode;
 }
 function MainLayout({ children }: MainLayoutProps) {
-  return <div className="main-layout flex-1 bg-[#EDEDED] border-2 border-white rounded-[1.5rem] overflow-auto">{children}</div>
+  return <div className="main-layout flex-1 bg-[#EDEDED] border-2 border-white rounded-[1.5rem] overflow-visible">{children}</div>
 }
 
 export default MainLayout;
